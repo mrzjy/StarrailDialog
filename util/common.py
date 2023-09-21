5d466b977b3f20fe126de4efe2a255e2ab7b70c5
jiayi <jiayi.zhang@ushow.media>
2023-09-21 18:05:31 +0800

    feat: restructure

--- util/common.py
+++ util/common.py
@@ -56,3 +56,106 @@ def get_speaker_content(
         print(f"warning: {sent_id} not found in text hash map")
         return None
     return {"role": speaker, "content": sentence}
+
+
+def parse_on_sequence(info: dict, map_hash_to_text: dict, map_sent_id_to_hash_info: dict) -> list:
+    sessions = []
+    for sequence in info["OnStartSequece"]:
+        session = []
+        for element in sequence["TaskList"]:
+            if element["$type"] == "RPG.GameCore.PlayAndWaitSimpleTalk":
+                sub_session = {"type": element["$type"], "conversations": []}
+                for simple_talk in element["SimpleTalkList"]:
+                    speaker_content = get_speaker_content(
+                        simple_talk["TalkSentenceID"],
+                        map_hash_to_text,
+                        map_sent_id_to_hash_info,
+                    )
+                    if speaker_content:
+                        simple_talk["role"] = speaker_content["role"]
+                        simple_talk["content"] = speaker_content["content"]
+                        simple_talk["type"] = "PlayAndWaitSimpleTalk"
+                        sub_session["conversations"].append(simple_talk)
+                if sub_session["conversations"]:
+                    session.append(sub_session)
+            elif element["$type"] == "RPG.GameCore.PlayOptionTalk":
+                sub_session = {"type": element["$type"], "options": []}
+                for option in element["OptionList"]:
+                    speaker_content = get_speaker_content(
+                        option["TalkSentenceID"],
+                        map_hash_to_text,
+                        map_sent_id_to_hash_info,
+                    )
+                    option["next_TalkSentenceID"] = re.search(
+                        r"TalkSentence_(?P<sent_id>\d+)",
+                        option["TriggerCustomString"],
+                    )["sent_id"]
+                    del option["TriggerCustomString"]
+                    del option["OptionIconType"]
+                    option["role"] = speaker_content["role"]
+                    option["content"] = speaker_content["content"]
+                    sub_session["options"].append(option)
+                if sub_session["options"]:
+                    session.append(sub_session)
+            elif element["$type"] in {
+                "RPG.GameCore.WaitCustomString",
+                "RPG.GameCore.TriggerCustomString",
+            }:
+                try:
+                    sent_id = int(
+                        re.search(
+                            r"TalkSentence_(?P<sent_id>\d+)",
+                            element["CustomString"]["Value"],
+                        )["sent_id"]
+                    )
+                except TypeError:
+                    sent_id = element["CustomString"]["Value"]
+                sub_session = {
+                    "type": element["$type"],
+                    "TalkSentenceID": sent_id,
+                }
+                session.append(sub_session)
+            elif element["$type"] == "RPG.GameCore.EndPerformance":
+                sub_session = {"type": "RPG.GameCore.EndPerformance"}
+                if session:
+                    session.append(sub_session)
+        if session:
+            sessions.append(session)
+
+    return sessions
+
+
+def merge_on_sequence_sessions(sessions: list):
+    merged_sessions = []
+    merged_session = []
+    for session in sessions:
+        for i, sub_session in enumerate(session):
+            if (
+                    merged_session
+                    and merged_session[-1]["type"] == "RPG.GameCore.WaitCustomString"
+            ):
+                if (
+                        sub_session["type"] == "RPG.GameCore.PlayAndWaitSimpleTalk"
+                        and sub_session["conversations"][0]["TalkSentenceID"]
+                        == merged_session[-1]["TalkSentenceID"]
+                ):
+                    del merged_session[-1]
+
+            if (
+                    merged_session
+                    and sub_session["type"] == "RPG.GameCore.TriggerCustomString"
+            ):
+                merged_session[-1]["next_TalkSentenceID"] = sub_session[
+                    "TalkSentenceID"
+                ]
+                continue
+
+            merged_session.append(sub_session)
+
+        if (
+                merged_session
+                and merged_session[-1]["type"] == "RPG.GameCore.EndPerformance"
+        ):
+            merged_sessions.append(merged_session)
+            merged_session = []
+    return merged_sessions
--- util/train_visitor_util.py
+++ util/train_visitor_util.py
@@ -1,9 +1,8 @@
 import glob
 import json
 import os
-import re
 
-from util.common import get_speaker_content
+from util.common import parse_on_sequence, merge_on_sequence_sessions
 
 
 def get_train_visitor(
@@ -20,106 +19,17 @@ def get_train_visitor(
     ):
         with open(file, "r", encoding="utf-8") as f:
             info = json.load(f)
-            for sequence in info["OnStartSequece"]:
-                session = []
-                for element in sequence["TaskList"]:
-                    if element["$type"] == "RPG.GameCore.PlayAndWaitSimpleTalk":
-                        sub_session = {"type": element["$type"], "conversations": []}
-                        for simple_talk in element["SimpleTalkList"]:
-                            speaker_content = get_speaker_content(
-                                simple_talk["TalkSentenceID"],
-                                map_hash_to_text,
-                                map_sent_id_to_hash_info,
-                            )
-                            if speaker_content:
-                                simple_talk["role"] = speaker_content["role"]
-                                simple_talk["content"] = speaker_content["content"]
-                                simple_talk["type"] = "PlayAndWaitSimpleTalk"
-                                sub_session["conversations"].append(simple_talk)
-                        if sub_session["conversations"]:
-                            session.append(sub_session)
-                    elif element["$type"] == "RPG.GameCore.PlayOptionTalk":
-                        sub_session = {"type": element["$type"], "options": []}
-                        for option in element["OptionList"]:
-                            if speaker_content:
-                                speaker_content = get_speaker_content(
-                                    option["TalkSentenceID"],
-                                    map_hash_to_text,
-                                    map_sent_id_to_hash_info,
-                                )
-                                option["next_TalkSentenceID"] = re.search(
-                                    r"TalkSentence_(?P<sent_id>\d+)",
-                                    option["TriggerCustomString"],
-                                )["sent_id"]
-                                del option["TriggerCustomString"]
-                                del option["OptionIconType"]
-                                option["role"] = speaker_content["role"]
-                                option["content"] = speaker_content["content"]
-                                sub_session["options"].append(option)
-                        if sub_session["options"]:
-                            session.append(sub_session)
-                    elif element["$type"] in {
-                        "RPG.GameCore.WaitCustomString",
-                        "RPG.GameCore.TriggerCustomString",
-                    }:
-                        try:
-                            sent_id = int(
-                                re.search(
-                                    r"TalkSentence_(?P<sent_id>\d+)",
-                                    element["CustomString"]["Value"],
-                                )["sent_id"]
-                            )
-                        except TypeError:
-                            sent_id = element["CustomString"]["Value"]
-                        sub_session = {
-                            "type": element["$type"],
-                            "TalkSentenceID": sent_id,
-                        }
-                        session.append(sub_session)
-                    elif element["$type"] == "RPG.GameCore.EndPerformance":
-                        sub_session = {"type": "RPG.GameCore.EndPerformance"}
-                        if session:
-                            session.append(sub_session)
-                if session:
-                    sessions.append(session)
+            sessions.extend(
+                parse_on_sequence(info, map_hash_to_text, map_sent_id_to_hash_info)
+            )
 
     output_dir = os.path.join("data", "dialogues", lang)
     os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, "train_visitor.jsonl")
-    count = 0
     with open(output_path, "w", encoding="utf-8") as f:
-        merged_session = []
-        for session in sessions:
-            for i, sub_session in enumerate(session):
-                if (
-                    merged_session
-                    and merged_session[-1]["type"] == "RPG.GameCore.WaitCustomString"
-                ):
-                    if (
-                        sub_session["type"] == "RPG.GameCore.PlayAndWaitSimpleTalk"
-                        and sub_session["conversations"][0]["TalkSentenceID"]
-                        == merged_session[-1]["TalkSentenceID"]
-                    ):
-                        del merged_session[-1]
-
-                if (
-                    merged_session
-                    and sub_session["type"] == "RPG.GameCore.TriggerCustomString"
-                ):
-                    merged_session[-1]["next_TalkSentenceID"] = sub_session[
-                        "TalkSentenceID"
-                    ]
-                    continue
-
-                merged_session.append(sub_session)
-
-            if (
-                merged_session
-                and merged_session[-1]["type"] == "RPG.GameCore.EndPerformance"
-            ):
-                print(json.dumps(merged_session, ensure_ascii=False), file=f)
-                merged_session = []
-                count += 1
-                if count >= max_count > 0:
-                    break
+        merged_sessions = merge_on_sequence_sessions(sessions)
+        for count, merged_session in enumerate(merged_sessions):
+            print(json.dumps(merged_session, ensure_ascii=False), file=f)
+            if count + 1 >= max_count > 0:
+                break
     print("Train_visitor: Total num of dialogues:", count)
